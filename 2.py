957fce730f7b658aa18236484db1c9a752b2a3c5
Deraapriani <aprianidera6@gmail.com>
2026-06-12 21:18:36 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,58 @@
+import dera051
+
+A = dera051.buat_matriks([
+    [5, 2],
+    [3, 1]
+])
+
+B = dera051.buat_matriks([
+    [4, 6],
+    [2, 5]
+])
+
+print("Matriks A")
+dera051.tampilkan_matriks(A)
+
+print("\nMatriks B")
+dera051.tampilkan_matriks(B)
+
+print("\nHasil Perkalian A x B")
+hasil = dera051.perkalian_matriks(A, B)
+dera051.tampilkan_matriks(hasil)
+
+print("\nTranspose Matriks A")
+hasil_transpose = dera051.transpose_matriks(A)
+dera051.tampilkan_matriks(hasil_transpose)
+
+print("\nDeterminan Matriks A")
+print(dera051.determinan_2x2(A))
+
+import dera051
+
+# Matriks 3x3 pertama
+A = dera051.buat_matriks([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+
+# Matriks 3x3 kedua
+B = dera051.buat_matriks([
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+])
+
+print("Matriks A")
+dera051.tampilkan_matriks(A)
+
+print("\nMatriks B")
+dera051.tampilkan_matriks(B)
+
+print("\nHasil Perkalian A x B")
+hasil = dera051.perkalian_matriks(A, B)
+dera051.tampilkan_matriks(hasil)
+
+print("\nTranspose Matriks A")
+hasil_transpose = dera051.transpose_matriks(A)
+dera051.tampilkan_matriks(hasil_transpose)
